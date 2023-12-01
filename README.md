0235db61068a7e1a93a0fa3cca389deb4af0beec
9oelM <hj923@hotmail.com>
2023-12-01 12:24:38 +0800

    chore: init repository

---
+++ README.md
@@ -0,0 +1,12 @@
+# notion-scrape-example
+
+This repository serves as the simplest example setup of how to use [@graphcentral/notion-graph-scraper](https://github.com/graphcentral/notion).
+
+## How to run
+
+1. Install node
+2. `npm start`
+
+## Example scraped file
+
+`test0.json` serves as an example of a scraped file.
---
+++ index.js
@@ -0,0 +1,26 @@
+import { NotionGraph } from "@graphcentral/notion-graph-scraper"
+import fs from "fs"
+/**
+ * example of how to use `@graphcentral/notion-graph-scraper`
+ */
+;(async () => {
+  const notionGraph = new NotionGraph({
+    maxDiscoverableNodes: 2000,
+    maxDiscoverableNodesInOtherSpaces: 2000,
+    verbose: true,
+  })
+  const graph = await notionGraph.buildGraphFromRootNode(
+    // notion help page
+    `95fcfe03257541c5aaa21dd43bdbc381`
+  )
+  console.log(graph.nodes.length)
+  console.log(graph.links.length)
+  await new Promise((resolve, reject) => {
+    fs.writeFile(`test0.json`, JSON.stringify(graph), (err) => {
+      if (err) reject(err)
+      else resolve(``)
+    })
+  });
+
+  process.exit(0)
+})()
